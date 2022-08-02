6c98ddc3de406b5c043fccfd13eb55989e694423
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-02 22:24:06 +0300

    Defined helper constants

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ *
+ * @ignore
+ */
+export default Object.freeze( {
+	ALLOWED_RICHTEXT_FORMATS: [ 'core/bold', 'core/italic' ],
+	IMAGE_MEDIA_TYPE: 'image',
+	VIDEO_MEDIA_TYPE: 'video',
+} );
