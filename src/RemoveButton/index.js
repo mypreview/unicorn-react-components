7d1b5b2e647507500a7d462f253e7e64d9bb9468
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-11 14:03:41 +0300

    Component: Remove Button

---
+++ src/RemoveButton/index.js
@@ -0,0 +1,68 @@
+/**
+ * External dependencies
+ *
+ * @ignore
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ *
+ * @ignore
+ */
+import { Button } from '@wordpress/components';
+import { ifCondition } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import { trash } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ *
+ * @ignore
+ */
+import { componentClassName } from '../utils';
+
+/**
+ * A wrapper component around the "Button" component provided by the core.
+ *
+ * This component has been created to represent removing items from a list
+ * with consideration for ease of styling and following D.R.Y principles.
+ *
+ * @function
+ * @since 	   1.2.0
+ * @param      {Object}         props             Component properties.
+ * @param  	   {JSX.Element}    props.children    Any React element or elements can be passed as children. They will be rendered within the wrapper.
+ * @param  	   {Function}    	props.onClick     Callback function to for processing click events on the button component.
+ * @return     {JSX.Element}                      Component to render.
+ * @example
+ *
+ * <RemoveButton onClick={ handleOnClickRemove } />
+ */
+function RemoveButton( { children, onClick, ...otherProps } ) {
+	return (
+		<Button
+			className={ componentClassName( 'remove-button' ) }
+			icon={ trash }
+			isDestructive
+			label={ __( 'Remove' ) }
+			onClick={ onClick }
+			showTooltip
+			tooltipPosition="top center"
+			{ ...otherProps }
+		>
+			{ children }
+		</Button>
+	);
+}
+
+RemoveButton.propTypes = {
+	children: PropTypes.element,
+	onClick: PropTypes.func,
+};
+
+RemoveButton.defaultProps = {
+	children: undefined,
+	onClick: () => {},
+};
+
+export default ifCondition( ( { doRender } ) => Boolean( doRender ) )( RemoveButton );
