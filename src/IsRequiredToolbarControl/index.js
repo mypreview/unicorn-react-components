ae62eb25e15ecd39e19ff227ef71c11afcb1cf99
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:38:27 +0300

    Renamed

--- src/IsRequiredToolbarControl/index.js
+++ src/IsRequiredToolbarControl/index.js
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
  *
  * @ignore
  */
+import { BlockControls } from '@wordpress/block-editor';
 import { ToolbarButton, ToolbarGroup } from '@wordpress/components';
 import { ifCondition } from '@wordpress/compose';
 
@@ -22,37 +23,42 @@ import { Icon } from '../';
 import Constants from './constants';
 
 /**
- * A ToolbarButton that is used to toggle the state of the requirement for a control.
+ * A Boolean attribute indicating that an option with a non-empty string value must be entered/selected.
  *
  * @function
  * @since 	   1.4.0
  * @param      {Object}         props            Component properties.
+ * @param      {string}         props.group      Block toolbar group name.
  * @param      {string}         props.label      Label property as the tooltip.
  * @param  	   {Function}       props.onClick    Callback function for processing click events on the button component.
  * @param 	   {boolean} 	    props.value      The current state of the button to be active or inactive.
  * @return     {JSX.Element}                     Component to render.
  * @example
  *
- * <IsRequiredToolbarButton onClick={ () => setAttributes( { isRequired: ! isRequired } ) } value={ isRequired } />
+ * <IsRequiredToolbarControl onClick={ () => setAttributes( { isRequired: ! isRequired } ) } value={ isRequired } />
  */
-function IsRequiredToolbarButton( { label, onClick, value } ) {
+function IsRequiredToolbarControl( { group, label, onClick, value } ) {
 	return (
-		<ToolbarGroup>
-			<ToolbarButton icon={ <Icon stroke="currentColor" d={ Constants.D } /> } isActive={ Boolean( value ) } label={ label } onClick={ onClick } />
-		</ToolbarGroup>
+		<BlockControls group={ group }>
+			<ToolbarGroup>
+				<ToolbarButton icon={ <Icon stroke="currentColor" d={ Constants.D } /> } isActive={ Boolean( value ) } label={ label } onClick={ onClick } />
+			</ToolbarGroup>
+		</BlockControls>
 	);
 }
 
-IsRequiredToolbarButton.propTypes = {
+IsRequiredToolbarControl.propTypes = {
+	group: PropTypes.string,
 	label: PropTypes.string,
 	onClick: PropTypes.func,
 	value: PropTypes.bool,
 };
 
-IsRequiredToolbarButton.defaultProps = {
+IsRequiredToolbarControl.defaultProps = {
+	group: 'other',
 	label: undefined,
 	onClick: () => {},
 	value: false,
 };
 
-export default ifCondition( ( { doRender } ) => Boolean( doRender ) )( IsRequiredToolbarButton );
+export default ifCondition( ( { doRender } ) => Boolean( doRender ) )( IsRequiredToolbarControl );
