ab5432a6aebf49ec57404a4679c86ccac2792b05
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-21 20:46:07 +0300

    Exported path to the Icon

---
+++ src/IsRequiredToolbarButton/constants.js
@@ -0,0 +1,10 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ *
+ * @ignore
+ */
+export default Object.freeze( {
+	D: 'M8.23118 8L16 16M8 16L15.7688 8 M6.5054 11.893L17.6567 11.9415M12.0585 17.6563L12 6.5',
+} );
