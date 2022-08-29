9448c1fb94f4a3ecc85637bd8ea6563d4ad48ebc
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-30 01:17:35 +0300

    Defined constant messages object

---
+++ src/MultiSelect/constants.js
@@ -0,0 +1,23 @@
+/**
+ * WordPress dependencies
+ *
+ * @ignore
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ *
+ * @ignore
+ */
+export default Object.freeze( {
+	MESSAGES: {
+		clear: __( 'Clear all' ),
+		noResults: __( 'No results found for your search term' ),
+		search: __( 'Search for items to display' ),
+		selectAll: __( 'Select all' ),
+		selected: __( 'item(s) selected' ),
+	},
+} );
