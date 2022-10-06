2f033ac2984e77198eaf05c2310c3b20a5148b49
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-10-06 16:36:37 +0300

    Define constants for the component

---
+++ src/ColorPicker/constants.js
@@ -0,0 +1,19 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ *
+ * @ignore
+ */
+export default Object.freeze( {
+	classNames: {
+		background: 'has-background',
+		link: 'has-link-color',
+		text: 'has-text-color',
+	},
+	context: {
+		background: 'background-color',
+		link: 'link',
+		text: 'color',
+	},
+} );
